926a5654c197d7e498fc5be1d31661040a0d225a
Sebastian Winter <61143274+lovesitecore@users.noreply.github.com>
2022-09-06 15:23:27 +0200

    Update input.php

---
+++ src/modules/2-images-in-a-row/input.php
@@ -0,0 +1,24 @@
+<fieldset class="form-horizontal">
+    <div class="form-group">
+        
+        <div class="col-12" style="padding: 10px; ">
+<label class="control-label">Überschrift 1</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[1]" value="REX_VALUE[1]">
+
+<label class="control-label">Bild 1</label>
+            REX_MEDIA[id="3" widget="1"]
+			
+        </div>
+
+ <div class="col-12" style="padding: 10px;"">
+<label class="control-label">Überschrift 2</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[4]" value="REX_VALUE[4]">
+
+<label class="control-label">Bild 2</label>
+            REX_MEDIA[id="6" widget="1"]
+			
+        </div>
+
+
+    </div>
+</div>
