0beaca88923d6b07a765da38ba5163ca843d69de
Sebastian Winter <61143274+lovesitecore@users.noreply.github.com>
2022-09-06 15:24:33 +0200

    Update output.php

---
+++ src/modules/3-download-cards/output.php
@@ -0,0 +1,39 @@
+<?php if(REX_VALUE[1] != ''){ ?>
+<div class="container">
+    <div class="row" style ="margin-top: 20px">
+    <div class="col-12 col-md-4">
+        <div class="card" >
+        <img class="card-img-top" src="./media/REX_MEDIA[3]" alt="Card image cap" style ="width: 100%;height: 240px;object-fit: cover;object-position: 100% 0;">
+        <div class="card-body" style="margin-left: 0px;">
+            <h5 class="card-title">REX_VALUE[1]</h5>
+            <p class="card-text">REX_VALUE[2]</p>
+            <a href="./media/REX_MEDIA[10]" class="btn btn-primary"  target="_blank">Download</a>
+        </div>
+        </div>
+</div>
+<?php } ?>
+<?php if(REX_VALUE[4] != ''){ ?>
+<div class="col-12 col-md-4">
+    <div class="card" >
+    <img class="card-img-top" src="./media/REX_MEDIA[6]" alt="Card image cap" style ="width: 100%;height: 240px;object-fit: cover;object-position: 100% 0;">
+    <div class="card-body" style="margin-left: 0px;">
+        <h5 class="card-title">REX_VALUE[4]</h5>
+        <p class="card-text">REX_VALUE[5]</p>
+        <a href="./media/REX_MEDIA[1]" class="btn btn-primary" target="_blank">Download</a>
+    </div>
+    </div>
+</div>
+<?php } ?>
+<?php if(REX_VALUE[7] != ''){ ?>
+<div class="col-12 col-md-4">
+    <div class="card" >
+    <img class="card-img-top" src="./media/REX_MEDIA[9]" alt="Card image cap" style ="width: 100%;height: 240px;object-fit: cover;object-position: 100% 0;">
+    <div class="card-body" style="margin-left: 0px;">
+        <h5 class="card-title">REX_VALUE[7]</h5>
+        <p class="card-text">REX_VALUE[8]</p>
+        <a href="./media/REX_MEDIA[2]" class="btn btn-primary"  target="_blank">Download</a>
+    </div>
+    </div>
+    </div>  
+</div>
+<?php } ?>
