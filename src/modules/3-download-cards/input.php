0a71b7977cba2546899b059c0324f634d90967d0
Sebastian Winter <61143274+lovesitecore@users.noreply.github.com>
2022-09-06 15:24:22 +0200

    Update input.php

---
+++ src/modules/3-download-cards/input.php
@@ -0,0 +1,37 @@
+<fieldset class="form-horizontal">
+    <div class="form-group">
+        
+        <div class="col-12" style="padding: 10px; ">
+<label class="control-label">Überschrift 1</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[1]" value="REX_VALUE[1]">
+<label class="control-label">Text 1</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[2]" value="REX_VALUE[2]">
+<label class="control-label">Bild 1</label>
+            REX_MEDIA[id="3" widget="1"]
+			<label class="control-label">Download Document 1</label>
+            REX_MEDIA[id="10" widget="1"]
+        </div>
+
+ <div class="col-12" style="padding: 10px;"">
+<label class="control-label">Überschrift 2</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[4]" value="REX_VALUE[4]">
+<label class="control-label">Text 2</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[5]" value="REX_VALUE[5]">
+<label class="control-label">Bild 2</label>
+            REX_MEDIA[id="6" widget="1"]
+			<label class="control-label">Download Document 2</label>
+            REX_MEDIA[id="1" widget="1"]
+        </div>
+
+ <div class="col-12" style="padding: 10px; "">
+<label class="control-label">Überschrift 3</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[7]" value="REX_VALUE[7]">
+<label class="control-label">Text 3</label>
+            <input class="form-control" type="text" name="REX_INPUT_VALUE[8]" value="REX_VALUE[8]">
+<label class="control-label">Bild 3</label>
+            REX_MEDIA[id="9" widget="1"]
+			<label class="control-label">Download Document 3</label>
+            REX_MEDIA[id="2" widget="1"]
+        </div>
+    </div>
+</div>
