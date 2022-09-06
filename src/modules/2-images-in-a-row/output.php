509a07f059417bcc7bec61dc31c2222a1da80b46
Sebastian Winter <61143274+lovesitecore@users.noreply.github.com>
2022-09-06 15:23:38 +0200

    Update output.php

---
+++ src/modules/2-images-in-a-row/output.php
@@ -0,0 +1,26 @@
+<?php if(REX_VALUE[1] != ''){ ?>
+<div class="container">
+    <div class="row" style ="margin-top: 20px">
+    <div class="col-12 col-md-6">
+        <div class="card" >
+        <img class="card-img-top" src="./media/REX_MEDIA[3]" alt="Card image cap" style ="width: 100%;object-fit: cover;object-position: 100% 0;">
+        <div class="card-body" style="margin-left: 0px;">
+            <h5 class="card-title">REX_VALUE[1]</h5>
+            
+        </div>
+        </div>
+</div>
+<?php } ?>
+<?php if(REX_VALUE[4] != ''){ ?>
+<div class="col-12 col-md-6">
+    <div class="card" >
+    <img class="card-img-top" src="./media/REX_MEDIA[6]" alt="Card image cap" style ="width: 100%;object-fit: cover;object-position: 100% 0;">
+    <div class="card-body" style="margin-left: 0px;">
+        <h5 class="card-title">REX_VALUE[4]</h5>
+   
+    </div>
+    </div>
+</div>
+<?php } ?>
+
+</div>
